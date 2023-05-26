0b967a93d8f8850415f1d27a22fb21e2c8f7f15b
tiksharsh <tiksharsh@icloud.com>
2023-05-26 23:39:26 +0530

    added split_data code

--- src/components/get_data.py
+++ src/components/get_data.py
@@ -1,6 +1,8 @@
 ## read params
 ## process
 ### return dataframe
+# https://www.analyticsvidhya.com/blog/2021/06/mlops-tracking-ml-experiments-with-data-version-control/
+# https://github.com/amitvkulkarni/Versioning-ML-Models-datasets-With-DVC/blob/main/src/get_data.py
 
 import os
 import yaml
